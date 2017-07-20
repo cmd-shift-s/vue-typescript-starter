f3608bc7075c4dcd5ced7e0ac4e63976fe209402
gongzza <gz.us.to@gmail.com>
2017-07-20 13:44:56 +0900

    Pass lint

--- src/components/Greet.vue
+++ src/components/Greet.vue
@@ -11,7 +11,7 @@
 
 <script lang="ts">
 import Vue from 'vue'
-import {Component, Prop} from 'vue-property-decorator'
+import { Component, Prop } from 'vue-property-decorator'
 
 @Component
 export default class Greet extends Vue {
